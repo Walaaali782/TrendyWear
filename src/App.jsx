d6ac11b8ff805b3c4928908d4b3db527fbf23775
Walaa Ali <105892469+Walaaali782@users.noreply.github.com>
2025-05-13 21:40:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ import Forgetpass from "./Pages/2-Auth/Forgetpass";
 import SendCode from "./Pages/2-Auth/SendCode";
 import AllPage from "./Pages/3-Home/Home";
 
-// import ProductDetail from "./pages/ProductDetail";
-// import Cart from "./pages/Cart";
-// import Shipping from "./pages/Shipping";
 
 
+
+import Catogry from "./Pages/Catogry";
+import ProductDetail from "./Pages/ProductDetail";
+import Cart from "./Pages/Cart";
+import Shipping from "./Pages/Shipping";
+import OrderSuccess from "./Pages/OrderSuccess";
+import Checkout from "./Pages/Checkout";
+import Rev from "./Pages/Rev";
+import Block from "./Pages/Block";
 
 
 
@@ -30,14 +36,23 @@ function App() {
           <Route path="/login/" element={<Login />} /> 
           <Route path="/forgetpassword/" element={<Forgetpass />} /> 
           <Route path="/sendcode/" element={<SendCode />} /> 
- 
-{/*         <Route path="/product" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/shipping" element={<Shipping />} /> */}
+
 
 
           <Route path="/Home/" element={<AllPage />} /> 
   
+
+        <Route path="/Catogry" element={<Catogry />} />
+        <Route path="/product" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
+
+
+<Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/checkout" element={<Checkout />} />
+
+<Route path="/Rev/" element={<Rev />} /> 
+            <Route path="/Block/" element={<Block />} />
 
           </Routes>
           </Router>
